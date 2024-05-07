b4a7cacf521671b57dbfcf51095ba14afe898582
Anni0p <anni@Anni0p.Anni0p>
2024-05-07 20:33:08 +0530

    Logic Building

---
+++ program85.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    for (iCnt = -iNo; iCnt<=-1; iCnt++)
+    {
+        
+        printf("%d\t%d\t",iCnt,-iCnt);
+        
+    }
+}
+
+int main()
+{
+    
+    int iValue = 0;
+    printf("Enter Number : ");
+    scanf("%d", &iValue);
+    Display(iValue);
+    return 0;
+}
