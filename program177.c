0ad17b86e189c7cfde9b9de6cbbc6a58735a597e
Anni0p <b.c114303@gmail.com>
2024-06-03 21:41:22 +0530

    Logic Building

---
+++ program177.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+int LastOccurence(char *str, char ch)    
+{
+    
+    int iCnt = 0;
+    while (*str != '\0')
+    {
+        if(*str == ch)
+        {
+           iCnt++;
+        }
+        
+        str++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char cValue = '\0';
+    char Arr[100];
+    int iRet = 0;
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n]s",Arr);
+
+    printf("Enter The Character that you want to search\n");
+    scanf(" %c", &cValue);
+    
+    iRet = LastOccurence(Arr, cValue);
+
+    printf("The Frequency is %d\n",iRet);
+    return 0;
+}
