900f174d7a8c2383fd610fec4f107896911edf38
anniop <b.c114303@gmail.com>
2024-07-05 22:41:47 +0530

    Logic Building

---
+++ File_Handling/File11.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+
+int main()
+{
+    int fd = 0; 
+    char FileName[30];
+    char Buffer[100] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter the file name that you want to open: \n");
+    scanf("%s", FileName);
+
+    fd = open(FileName, O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return 0;
+    }
+
+    while(iRet = read(fd,Buffer,sizeof(Buffer)) != 0)
+    {
+        printf("%s\n",Buffer);
+        memset(Buffer,0,sizeof(Buffer));
+    }
+
+    close(fd); 
+
+    return 0;
+}
